0caa5899592a6d16cb82888769bba2c54319e157
Victor Montenegro <jvcmontenegro67@gmail.com>
2025-09-24 20:28:11 -0300

    adjusting

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+output "cluster_id" {
+  value = module.eks.cluster_id
 }
 
-output "cluster_certificate_authority" {
-  value = module.eks.cluster_certificate_authority[0].data
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
 }
--- providers.tf
+++ providers.tf
@@ -2,12 +2,17 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Data source para obter dados do cluster EKS
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority[0].data)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
