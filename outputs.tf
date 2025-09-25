309fa19721c6b0a00ab2e0ffad7ad1d1720d0e6f
Victor Montenegro <jvcmontenegro67@gmail.com>
2025-09-25 19:47:27 -0300

    add outputs tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "cluster_id" {
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
