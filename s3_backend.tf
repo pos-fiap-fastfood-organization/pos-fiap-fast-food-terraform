17233ef8c29a587c8c740d6498186b14283aa175
Victor Montenegro <jvcmontenegro67@gmail.com>
2025-09-30 09:38:41 -0300

    adjusting

--- s3_backend.tf
+++ s3_backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "fiap-fastfood-terraform-state"
+    bucket  = "fiap-fastfood-terraform-state-lgrando"
     key     = "eks/terraform.tfstate"
-    region  = "us-east-2"
+    region  = "us-east-1"
     encrypt = true
   }
 }
